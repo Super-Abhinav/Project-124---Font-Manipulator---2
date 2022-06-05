98bc57d553e6cd65136e4631c725dc45365b2b2c
Abhinav Anand <85018022+Super-Abhinav@users.noreply.github.com>
2022-06-05 11:53:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 function setup() {
     video = createCapture(VIDEO);
-    video.size(550,550);
+    video.size(400,400);
+    video.position(10,50);
 
-    canvas = createCanvas(550,550);
-    canvas.position(560,150);
+    canvas = createCanvas(700,500);
+    canvas.position(430,130);
 
     poseNet = ml5.poseNet(video,modelLoaded);
     poseNet.on('pose',gotPoses);
+}
+
+function draw(){
+    background("#5196e3");
 }
 
 function modelLoaded() {
